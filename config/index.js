5cef8be814679a2383ea8a5b0dfd850271b3db7a
hanyanjun <HYJ_hanyanjun@163.com>
2021-05-18 23:37:11 +0800

    fix: add file

--- config/index.js
+++ config/index.js
@@ -38,11 +38,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../'),
-    assetsSubDirectory: 'static',
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: './static',
     assetsPublicPath: '/',
 
     /**
